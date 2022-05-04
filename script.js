bfcf3287ace2f7e16278d58bca1735bdd049eaf8
Resonanc3 <79844632+Resonanc3@users.noreply.github.com>
2022-05-04 23:46:02 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "birthday" && b=="0505year")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
