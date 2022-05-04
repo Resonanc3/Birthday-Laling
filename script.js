24c255dd00eb6501b86b37ec32311780f7faf192
Resonanc3 <79844632+Resonanc3@users.noreply.github.com>
2022-05-04 23:21:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var a=prompt("Enter UserName");
  var b=prompt("Enter password");
-  	if(a== "Dubdub" && b=="12345")
+  	if(a== "birthday" && b=="0505year")
   	{
   	    window.location.href="start.html";
   	}
